3689ed46acc88e3778cb9a4e9a5f6ab8bebf9556
Aleksey Mladinskiy <alekseymladinskiy@gmail.com>
2025-03-28 21:47:08 +0300

    modified file

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,16 +3,33 @@ package ru.netology.nmedia
 import android.os.Bundle
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
-import ru.netology.nmedia.dto.Post
 import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import ru.netology.nmedia.dto.Post
 
+//import android.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var post: Post
-    private var shareCount = 9148
-    private var viewCount = 5500
+    private var shareCount =99999
+    private var viewCount = 5500000
+
+    //    private val viewModel: PostViewModel by viewModels()
+    private var post = Post(
+        id = 1,
+        author = "Нетология. Университет интернет-профессий будущего",
+        published = "21 мая в 18:36",
+        content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по" +
+                " онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и " +
+                "управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных" +
+                " профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила," +
+                " которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь" +
+                " встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+        like = 10999,
+        likedByMi = false
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,27 +37,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val author = getString(R.string.Netology_author)
-//        val published = getString(R.string.Netology_published)
-//        val content = getString(R.string.Netology_content)
 
-        post = Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            published = "21 мая в 18:36",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            like = 1099,
-            likedByMi = false
-        )
-
+//        viewModel.data.observe(this) { post ->
+        updateLikeButton()
+        updateCounts()
         binding.author.text = post.author
         binding.published.text = post.published
         binding.contentTextView.text = post.content
-        
-        updateLikeButton()
-        updateCounts()
+
+        binding.root.setOnClickListener {
+            Log.d("MainActivity", "Кликнул не по кнопке")
+            Toast.makeText(this, "Кликнул не по кнопке", Toast.LENGTH_SHORT).show()
+        }
+
 
         binding.likeImageView.setOnClickListener {
+            Log.d("MainActivity", "Кликнул лайк")
+            Toast.makeText(this, "Кликнул лайк", Toast.LENGTH_SHORT).show()
             post.likedByMi = !post.likedByMi
             if (post.likedByMi) {
                 post.like++
@@ -55,6 +68,20 @@ class MainActivity : AppCompatActivity() {
             shareCount++
             updateCounts()
         }
+
+        binding.menu.setOnClickListener {
+            Log.d("MainActivity", "Кликнул меню")
+
+            Toast.makeText(this, "Кликнул меню", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.avatar.setOnClickListener {
+            Log.d("MainActivity", "Кликнул аватар")
+
+            Toast.makeText(this, "Кликнул аватар", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
 
@@ -71,8 +98,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun formatCount(count: Int, context: Context): String {
         return when {
-            count >= 1000000 -> String.format(context.getString(R.string.format_million), count.toDouble() / 1000000)
-            count >= 1000 -> String.format(context.getString(R.string.format_thousand), count.toDouble() / 1000)
+            count >= 1_000_000 -> {
+                val million = count / 100_000
+                (million / 10.0).toString() + "M"
+            }
+
+            count >= 10_000 -> (count / 1000).toString() + "K"
+            count >= 1_000 -> {
+                val thousand = count / 100
+                (thousand / 10.0).toString() + "K"
+            }
+
             else -> count.toString()
         }
     }
